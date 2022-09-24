5a1667137a6bb5a23cb17cb6aa203e0f609b3e7d
Suyog Jadhav <suyogjadhav045@gmail.com>
2022-09-25 03:58:41 +0530

    completed search and advance search with show page by bablu and nana

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(express.urlencoded({ extended: true }))
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/Client/views'));
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+const { log } = require("console");
 app.use(methodOverride('_method'));
 
 
@@ -26,7 +27,8 @@ app.post('/', (req, res) => {
 
     if (typeof search.genre === 'string' || search.genre instanceof String) {
         var s = search.genre;
-    } else {
+    }
+    else {
         var s = "";
         for (let i of search.genre) {
             // console.log(i);
@@ -36,9 +38,10 @@ app.post('/', (req, res) => {
         }
         s = s.substring(1);
     }
+    search["page"] = 1;
     search.genre = s;
     console.log("Search[genre]2 :: ", search.genre);
-    // Number(search[min_idbm]);
+    search.min_idbm = Number(search.min_idbm);
     // min_idbm
 
     // console.log("Search :: ", search);
@@ -46,28 +49,60 @@ app.post('/', (req, res) => {
     const options = {
         method: 'GET',
         url: 'https://ott-details.p.rapidapi.com/advancedsearch',
-        params: {
-            search: search
-        },
+        params: search,
+        headers: {
+            'X-RapidAPI-Key': '03ffe1cadcmshe653975363c53a4p1ffa00jsn7d40601cb65f',
+            'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
+        }
+    };
+    // console.log("search :: \n", search);
+    // axios.request(options).then(function (response) {
+    //     console.log(response.data);
+    //     res.render("search.ejs", { search: response.data.results });
+    // }).catch(function (error) {
+    //     console.error(error);
+    // });
+
+
+
+
+    fs.readFile('data.json', (err, data) => {
+        if (err) throw err;
+        let d = JSON.parse(data);
+        // console.log(data);
+        res.render("search.ejs", { search: d.results });
+
+
+    });
+});
+
+
+app.post('/search', (req, res) => {
+    const { search } = req.body;
+    // console.log(search);
+    search["page"] = 1;
+
+    const options = {
+        method: 'GET',
+        url: 'https://ott-details.p.rapidapi.com/search',
+        params: search,
         headers: {
             'X-RapidAPI-Key': '03ffe1cadcmshe653975363c53a4p1ffa00jsn7d40601cb65f',
             'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
         }
     };
 
-    // axios.request(options).then(function (response) {
-    //     const { data } = response;
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+        res.render("search.ejs", { search: response.data.results });
+    }).catch(function (error) {
+        console.error(error);
+    });
 
-    //     // console.log("data :: ", data);
-    //     const dataJSON = JSON.stringify(data);
-    //     fs.writeFileSync('data.json', dataJSON);
 
-    // }).catch(function (error) {
-    //     console.error(error);
-    // });
+});
 
-    res.render("home.ejs");
-})
+
 
 app.listen(3000, () => {
     console.log('Listning on port 3000');
